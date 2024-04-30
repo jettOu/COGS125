5d1115fa61181c22c12d2d21bf50f4e243667ba2
jettOu <66858833+jettOu@users.noreply.github.com>
2024-04-30 02:27:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './shark.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ color: 'black' }}>
+          Jett's portfolio homepage
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Resume"
+          href="https://docs.google.com/document/d/1Wtb71jotiXjxGAhz_mg2CI1tPwaQOyH8VWmiCbcZGAs/edit?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Portfolio
         </a>
       </header>
     </div>
---
+++ src/route.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Switch, Route, Router } from "react-router-dom";
+
+export const route = () => {
+    return(
+        <Router>
+            <Switch>
+                <Route path = "/">
+
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
