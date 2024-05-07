6ffec3873a651e9fcf5ee112a9609912b4883471
jettOu <66858833+jettOu@users.noreply.github.com>
2024-05-06 17:23:36 -0700

    deployed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,52 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={shark} className="App-logo" alt="logo" />
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', left: '10px', }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', right: '10px', }} src={seaweed} className = "Seaweed" alt = "logo"/>
+        
+        <img style={{ position: 'absolute',
+          bottom: '10px', center: '10px', }} src={seaweed} className = "Seaweed" alt = "logo"/>
+        
+        <img style={{ position: 'absolute',
+          bottom: '10px', left:'100px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', right:'100px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', left:'200px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+        
+        <img style={{ position: 'absolute',
+          bottom: '10px', right:'200px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+        
+        <img style={{ position: 'absolute',
+          bottom: '10px', left:'300px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', right:'300px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', left:'400px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', right:'400px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', left:'500px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', right:'500px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', left:'600px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
+        <img style={{ position: 'absolute',
+          bottom: '10px', right:'600px' }} src={seaweed} className = "Seaweed" alt = "logo"/>
+
         <p className="words">
           Jett's portfolio homepage
         </p>
